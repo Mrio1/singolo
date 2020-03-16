ff499991100d77bc01d5caaa1142983382ad8b4c
Aliaksandr Zhur <sanchesmrio1@gmail.com>
2020-03-16 21:18:19 +0300

    feat: implement slider block

--- script.js
+++ script.js
@@ -1,8 +1,4 @@
 const navList = document.querySelector(".main_navigation--list");
-console.log(navList);
-
-const servicesLink = document.querySelector("a[href = '#portfolio']");
-console.log(servicesLink);
 
 let currentActiveNavLink;
 
@@ -18,3 +14,51 @@ navList.addEventListener('click', function (event) {
         }
     } 
 })
+
+
+///////////////SLIDER
+let leftArrow = document.querySelector('.slider--arrow_left');
+let rightArrow = document.querySelector('.slider--arrow_right');
+let sliderList = document.querySelector('.slider--list');
+console.log(leftArrow)
+
+let slides = document.querySelectorAll('.slider--item');
+let slidesLength = slides.length;
+let sliderOffsetLeft = slides[0].offsetLeft;
+let sliderCounter = 0;
+
+rightArrow.addEventListener('click', function(){
+    sliderCounter++;
+    if (sliderCounter >= slidesLength){
+        sliderCounter = 0;
+    }
+    sliderList.style.marginLeft = `${sliderOffsetLeft - 1020*sliderCounter}px`;
+    
+    console.log(sliderCounter);
+})
+
+leftArrow.addEventListener('click', function(){
+    sliderCounter--;
+    if (sliderCounter < 0){
+        sliderCounter = slidesLength - 1;
+    }
+    sliderList.style.marginLeft = `${sliderOffsetLeft - 1020*sliderCounter}px`;
+});
+
+for (let i = 0; i < slidesLength; i++){
+    let sliderItems = document.querySelectorAll(`.slider--item${i+1}_image`);
+    let sliderItemMode = true;
+    //console.log(sliderItem1[0].src.slice(0,-6))
+    sliderItems.forEach((element)=>{
+        element.addEventListener('click', function(){
+            if (sliderItemMode){
+                element.src = element.src.slice(0,-6)+'of.png';
+                sliderItemMode = false;
+            } else {
+                element.src = element.src.slice(0,-6)+'on.png';
+                sliderItemMode = true;
+            }
+            
+        })
+    })
+}
